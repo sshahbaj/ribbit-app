5ed064cf95c124a5cc43d217fb82139f02f4e479
sshahbaj <shahbaj007@gmail.com>
2017-09-12 23:11:21 +0530

    Add index view, home and buddies templates

--- ribbit/ribbit/urls.py
+++ ribbit/ribbit/urls.py
@@ -18,4 +18,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', 'ribbit_app.views.index'),
+    url(r'^login$', 'ribbit_app.views.login_view'),
+    url(r'^logout$', 'ribbit_app.views.logout_view'),
+    url(r'signup^$', 'ribbit_app.views.signup'),
 ]
--- ribbit/ribbit_app/views.py
+++ ribbit/ribbit_app/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from .forms import AuthenticateForm, UserCreateForm, RibbitForm
+from .models import Ribbit
 
-# Create your views here.
+
+def index(request, auth_form=None, user_form=None):
+    # User is logged in
+    if request.user.is_authenticated():
+        ribbit_form = RibbitForm()
+        user = request.user
+        ribbit_self = Ribbit.objects.filter(user=user.id)
+        ribbit_buddies = Ribbit.objects.filter(user__userprofile__in=user.profile.follows.all)
+        ribbits = ribbit_self | ribbit_buddies
+        context = {
+            'ribbit_form': ribbit_form,
+            'user': user,
+            'next_url': '/',
+        }
+
+        return render(request, 'buddies.html', context)
+
+    else:
+        # User is not logged in
+        auth_form = auth_form or AuthenticateForm()
+        user_form = user_form or UserCreateForm()
+        context = {
+            'auth_form': auth_form,
+            'user_form': user_form,
+        }
+
+        return render(request, 'home.html', context)
